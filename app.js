8eeecfcc3cefb13da33ca1d7a879db4f3b10e34b
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-07-01 13:59:08 -0400

    added array/ object structre for questions added start game function

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 console.log("trivia game ")
+
+function  startgame(){
+
+};
+  
+let data = [
+    {
+        question: " what color is the sky ",
+        ans:[
+            {
+                slot1:blue,
+            },
+            {
+                slot2:green
+            },
+            {
+                slot3:red
+            },
+            {
+                slot4:purple,
+            }
+            
+        ]
+    },
+    {
+        question: " what color is the sky ",
+        ans:[
+            {
+                slot1:blue,
+            },
+            {
+                slot2:green
+            },
+            {
+                slot3:red
+            },
+            {
+                slot4:purple,
+            }
+            
+        ]
+    }
+]
