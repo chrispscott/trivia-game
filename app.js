47c86d56bbb1aad66f6c14d65b60d1f2d71de595
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-07-04 12:52:41 -0400

    added the first round of questions created and array of objects to hold data

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
 console.log("trivia game ")
 
-function  startgame(){
-
-};
+// function  startgame(x){
+//   x = document.getElementById("txtbox").innerHTML=(this.ans[0])
+// };       
   
-let data = [
+let data =[
     {
-        question: " what color is the sky ",
-        ans:[
-            {
-                slot1:blue,
-            },
-            {
-                slot2:green
-            },
-            {
-                slot3:red
-            },
-            {
-                slot4:purple,
-            }
-            
-        ]
+        txt:'In what year was the first Harry Potter movie released',
+        slot1:2001,
+        slot2:1994,
+        slot3:2003,
+        slot4:1864
     },
     {
-        question: " what color is the sky ",
-        ans:[
-            {
-                slot1:blue,
-            },
-            {
-                slot2:green
-            },
-            {
-                slot3:red
-            },
-            {
-                slot4:purple,
-            }
-            
-        ]
-    }
-]
+        txt:'What is the name of Harry Potter\'s godfather',
+        slot1:"sirius black",
+        slot2:'Mat Damon',
+        slot3:'pikachu',
+        slot4:'Bernie Sanders'
+    },
+    {
+        txt:'Who usually decides which house at Hogwarts students are to belong to',
+        slot1:'The Sorting Hat',
+        slot2:'The student',
+        slot3:'Harry potter',
+        slot4:'Dennis Rodman'
+    },
+    {
+        txt:'What area lies to the west of Hogwarts Castle',
+        slot1:'The Forbidden Forest',
+        slot2:'Disney world',
+        slot3:'vegas',
+        slot4:'all of the above'
+    },
+    {
+        txt:'Which Harry Potter character used to own a Firebolt',
+        slot1:'Harry Potter',
+        slot2:'Donald Duck',
+        slot3:'The Power Puff Girls',
+        slot4:'Hermione Granger'
+    },
+    {
+        txt:' Begin round 2',
+        slot1:'next',
+        slot2:"next",
+        slot3:'next',
+        slot4:'next'
+    },
+
+]  
+console.log(data);
+//startgame()
